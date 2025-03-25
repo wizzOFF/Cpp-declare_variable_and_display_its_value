daad05baac1f8c3a9e1efd5b30c0c042070a632c
WizzOFF <maher_off@proton.me>
2025-03-25 20:04:52 +0100

    declare_variable_and_display_its_value.cpp: declare other variables

--- declare_variable_and_display_its_value.cpp
+++ declare_variable_and_display_its_value.cpp
@@ -5,5 +5,14 @@ using namespace std;
 int main()
 {
     int ageUtilisateur(16);
+
+    int nombreAmis(432);      //Le nombre d'amis de l'utilisateur
+
+    double pi(3.14159);
+
+    bool estMonAmi(true);    //Cet utilisateur est-il mon ami ?
+
+    char lettre('a');
+    
     return 0;
 }
